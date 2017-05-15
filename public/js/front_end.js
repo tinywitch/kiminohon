5962e03da01fd5473d2a374c632f0af7e576973d
Thuy Ninh <thuyninh.esc@gmail.com>
2017-05-15 06:45:10 +0000

    fix sth

--- public/js/front_end.js
+++
@@ -1,6 +0,0 @@
-
-if (typeof NProgress != 'undefined') {
-    $(document).on('page:fetch',   function() { NProgress.set(0.3); });
-    $(document).on('page:change',  function() { NProgress.done(); });
-    $(document).on('page:restore', function() { NProgress.remove(); });
-}
